ba6a8fe0d139d4454eb688ab01fde95aec56918c
gy0109 <1974326896@qq.com>
2019-04-02 21:15:28 +0800

    scrapy

---
+++ 4-2-Scrapy/02-scrapy创建于安装.py
@@ -0,0 +1,8 @@
+# 安装： sudo apt-get install scrapy 或者：pip install scrapy
+# 创建： scrapy startproject 项目名称
+
+# pip._in
+
+# Twisted 需要去网站下载  然后切换到下载目录下进行cmd安装， 安装过程中需要查看所需版本号和名称支持
+
+# [('cp37', 'cp37m', 'win32'), ('cp37', 'none', 'win32'), ('py3', 'none', 'win32'), ('cp37', 'none', 'any'), ('cp3', 'none', 'any'), ('py37', 'none', 'any'), ('py3', 'none', 'any'), ('py36', 'none', 'any'), ('py35', 'none', 'any'), ('py34', 'none', 'any'), ('py33', 'none', 'any'), ('py32', 'none', 'any'), ('py31', 'none', 'any'), ('py30', 'none', 'any')]
